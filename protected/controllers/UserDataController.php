eacbbb9663730aa8bbe0f905567cee9833c8cdd4
tennosys <tennosys@gmail.com>
2014-05-24 10:09:50 +0700

    db

--- protected/controllers/UserDataController.php
+++ protected/controllers/UserDataController.php
@@ -34,7 +34,7 @@ class UserDataController extends Controller {
             ),
             array('allow', // allow admin user to perform 'admin' and 'delete' actions
                 'actions' => array('admin', 'delete'),
-                'id' => array('M0509069'),
+                'users' => array('Teno Siwono'),
                 //'expression'=>'Yii::app()->user->isAdmin()',
             ),
             array('deny', // deny all users
